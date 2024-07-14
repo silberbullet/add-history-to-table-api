4760b0ad035dd5eea5487a379ae7acba1b45f43d
silberbullet <pkw961028@naver.com>
2024-07-14 13:55:18 +0900

    project init

---
+++ src/test/java/com/delivery/history/HistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delivery.history;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HistoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
